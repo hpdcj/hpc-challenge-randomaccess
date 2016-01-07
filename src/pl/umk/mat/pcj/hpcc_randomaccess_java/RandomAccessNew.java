c59caaac591d379ba8c5bf59aa85cbc3d2467747
Łukasz Górski <lukeg5@gmail.com>
2016-01-07 21:52:49 +0100

    działający randomaccess

--- src/pl/umk/mat/pcj/hpcc_randomaccess_java/RandomAccessNew.java
+++ src/pl/umk/mat/pcj/hpcc_randomaccess_java/RandomAccessNew.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.umk.mat.pcj.hpcc_randomaccess_java;
 
 import java.io.File;
@@ -15,7 +10,6 @@ import org.pcj.PCJ;
 import org.pcj.Shared;
 import org.pcj.StartPoint;
 import org.pcj.Storage;
-import static pl.umk.mat.pcj.hpcc_randomaccess_java.MPIRandomAccessHypercube2.HPCC_Table;
 
 /**
  *
@@ -24,7 +18,13 @@ import static pl.umk.mat.pcj.hpcc_randomaccess_java.MPIRandomAccessHypercube2.HP
 class RandomAccessNewStorage extends Storage {
 
     @Shared
-    int[] table;
+    private long[] table;
+
+    @Shared
+    private ArrayList<Long> receivedUpdates;
+
+    @Shared
+    private int okCells;
 }
 
 public class RandomAccessNew implements StartPoint {
@@ -33,8 +33,9 @@ public class RandomAccessNew implements StartPoint {
     int globalN;
     int logLocalN;
     int localN;
+    int logNumProcs;
     long updates;
-    RandomForRA random = new RandomForRA();
+    RandomForRA random;
     public static final int BUFFERED_UPDATES = 1024;
 
     public static void main(String[] args) {
@@ -51,136 +52,162 @@ public class RandomAccessNew implements StartPoint {
         double start = System.currentTimeMillis();
         performRandomAccess();
         double stop = System.currentTimeMillis();
+        PCJ.log("Starting verification");
+        verifyResultsLocally();
+        thread0VerifyAll();
+        if (PCJ.myId() == 0) {
+            System.out.println("Time: " + (stop - start) * 1e-9 + " s.");
+        }
     }
 
     private void initializeData() throws FileNotFoundException {
         Scanner in = new Scanner(new File("ra.config"));
         logN = in.nextInt();
-        logLocalN = logN - PCJ.threadCount();
+        logNumProcs = (int) (Math.log(PCJ.threadCount()) / Math.log(2));
+        logLocalN = logN - logNumProcs;
         localN = 1 << logLocalN;
         updates = 4 * localN;
         long[] table = new long[localN];
         for (int i = 0; i < localN; i++) {
-            table[i] = i + PCJ.myId() * logLocalN;
+            table[i] = i + PCJ.myId() * localN;
         }
         PCJ.putLocal("table", table);
+        random = new RandomForRA(PCJ.myId());
     }
 
     List<Long> send = new ArrayList<>();
     List<Long> keep = new ArrayList<>();
 
-    private int randomNumberToLocalPosition (long rand) {
+    private int randomNumberToLocalPosition(long rand) {
         return (int) (rand & (localN - 1));
     }
-    private List<Long> localUpdatesAndGenerateRemoteUpdates(int update) {
+
+    private List<Long> localUpdatesAndGenerateRemoteUpdates(int update, int CHUNK_SIZE) {
         List<Long> numbers = new ArrayList<>();
 
-        for (int k = 0; k < BUFFERED_UPDATES && update + k < updates; k++) {
+        for (int k = 0; k < CHUNK_SIZE && update + k < updates; k++) {
             long rand = random.nextLong();
-            int whichPe = (int) ((rand) >> logLocalN) & (PCJ.threadCount() - 1);
-
-            if (whichPe == PCJ.myId()) {
-                int localOffset = randomNumberToLocalPosition(rand);
-                HPCC_Table[(int) localOffset] ^= rand;
-            } else {
-                numbers.add(rand);
-            }
+            numbers.add(rand);
         }
         return numbers;
     }
 
     private void performRandomAccess() {
-        for (int update = 0; update < updates; update++) {
-            List<Long> updates = localUpdatesAndGenerateRemoteUpdates(update);
-            alltoallHypercube(updates);
+        for (int update = 0; update < updates; update += BUFFERED_UPDATES) {
+            List<Long> updateList = localUpdatesAndGenerateRemoteUpdates(update, BUFFERED_UPDATES);
+            updateList = alltoallHypercube(updateList);
+            performUpdates(updateList);
         }
     }
 
-    private void alltoallHypercube(List<Long> updates) {
-            //all-to-all hypercube personalized communication, per 
-            //http://www.sandia.gov/~sjplimp/docs/cluster06.pdf, p. 5.
-            for (int dimension = 0; dimension < Math.log(PCJ.threadCount()) / Math.log(2); dimension++) {
-                send.clear();
-                keep.clear();
-                int partner = (1 << dimension) ^ PCJ.myId();
+    private List<Long> alltoallHypercube(List<Long> updates) {
+        //all-to-all hypercube personalized communication, per 
+        //http://www.sandia.gov/~sjplimp/docs/cluster06.pdf, p. 5.
+        List<Long> list = updates;
+        for (int dimension = 0; dimension < logNumProcs; dimension++) {
+            int partner = (1 << dimension) ^ PCJ.myId();
+            PCJ.barrier(partner);
+            long mask = 1L << (logLocalN + dimension);
+            prepareUpdateLists(partner, list, mask);
+            sendListToPartner(partner);
+            PCJ.barrier(partner);
+            PCJ.waitFor("receivedUpdates");
+            receiveUpdates();
 
-                long mask = ((1L << dimension) << logLocalN);
+            list.clear();
+            list.addAll(keep);
+            keep.clear();
 
-                if (partner > PCJ.myId()) {
-                    Iterator<Long> iter = list.iterator();
-                    while (iter.hasNext()) {
-                        long x = iter.next();
-                        if ((mask & x) != 0) {
-                            send.add(x);
-                        } else {
-                            keep.add(x);
-                        }
-                    }
+        }
+        return list;
+    }
+
+    private void sendListToPartner(int partner) throws ClassCastException {
+        PCJ.put(partner, "receivedUpdates", send);
+        send.clear();
+    }
+
+    private void receiveUpdates() {
+        List<Long> receivedUpdates = PCJ.getLocal("receivedUpdates");
+
+        keep.addAll(receivedUpdates);
+
+    }
+
+    private void prepareUpdateLists(int partner, List<Long> updates1, long mask) {
+        if (partner > PCJ.myId()) {
+            for (long update : updates1) {
+                if ((update & mask) != 0) {
+                    send.add(update);
                 } else {
-                    Iterator<Long> iter = list.iterator();
-                    while (iter.hasNext()) {
-                        long x = iter.next();
-                        if ((mask & x) != 0) {
-                            keep.add(x);
-                        } else {
-                            send.add(x);
-                        }
-                    }
+                    keep.add(update);
                 }
-
-                long[] buff = new long[send.size() + 1];
-                buff[0] = send.size();
-//                String out = "";
-//                out += send.size() + " ";
-//                Iterator<Long> irr = send.iterator();
-//                while (irr.hasNext()) {
-//                    out += irr.next() + " ";
-//                }
-                // System.err.println (out);
-                Iterator<Long> iter = send.iterator();
-                int z = 1;
-                while (iter.hasNext()) {
-                    buff[z++] = iter.next();
-                }
-                send.clear();
-                if (timeBound && bailed[tparams.MyProc] == true) {
-                    buff[0] += 100_000;
-                }
-//                String sss = "Thr " + tparams.MyProc + " is putting to thread " + partner + " following data: { ";
-//                for (int g = 0; g <= buff[0]; g++) {
-//                    sss += buff[g] + " ";
-//                }
-//                System.err.println (sss);
-
-                if (timeBound == false || bailed[partner] == false) {
-                    PCJ.put(partner, "thr" + tparams.MyProc, buff);
-                    //               System.err.println (tparams.MyProc + " waiting for " + partner);
-                    if (timeBound && bailed[tparams.MyProc] == true) {
-                        bailed[partner] = true;
-                    }
-                    PCJ.waitFor("thr" + partner);
-                    //                System.err.println ("finished wait");
-                    long[] recv = PCJ.get("thr" + partner);
-                    if (timeBound) {
-                        if (recv[0] >= 100_000) {
-                            bailed[partner] = true;
-                            bailed[tparams.MyProc] = true;
-                            recv[0] -= 100_000;
-                        }
-                    }
-                    for (int zz = 1; zz <= recv[0]; zz++) {
-                        //  System.err.println (tparams.MyProc + " " + zz + " " + recv[0]);
-                        keep.add(recv[zz]);
-                    }
-                }
-
-                list.clear();
-                Iterator<Long> iter2 = keep.iterator();
-                while (iter2.hasNext()) {
-                    list.add(iter2.next());
-                }
-
             }
+        } else {
+            for (long update : updates1) {
+                if ((update & mask) != 0) {
+                    keep.add(update);
+                } else {
+                    send.add(update);
+                }
+            }
+        }
+    }
+
+    int whichPE(long pos) {
+        return (int) (pos >> logLocalN) & (PCJ.threadCount() - 1);
+    }
+
+    private void performUpdates(List<Long> updateList) {
+        for (long update : updateList) {
+            updateSingleCell(update);
+        }
+    }
+
+    private void updateSingleCell(long update) throws ClassCastException {
+        int localOffset = randomNumberToLocalPosition(update);
+        long val = PCJ.getLocal("table", localOffset);
+        val ^= update;
+        PCJ.putLocal("table", val, localOffset);
+    }
+
+    private void verifyResultsLocally() {
+        PCJ.barrier();
+
+        for (int PE = 0; PE < PCJ.threadCount(); PE++) {
+            RandomForRA random = new RandomForRA(PE);
+
+            for (int update = 0; update < updates; update++) {
+                long val = random.nextLong();
+                if (whichPE(val) == PCJ.myId()) {
+
+                    updateSingleCell(val);
+                }
+            }
+        }
+
+        long[] table = PCJ.getLocal("table");
+        int ok = 0;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] == i + PCJ.myId() * localN) {
+                ok++;
+            } else {
+                PCJ.log("Cell #" + i + " is " + table[i] + ", should be: " + (i + PCJ.myId() * localN));
+            }
+        }
+        PCJ.putLocal("okCells", ok);
+        PCJ.barrier();
+    }
+
+    private void thread0VerifyAll() {
+        if (PCJ.myId() == 0) {
+            for (int i = 0; i < PCJ.threadCount(); i++) {
+                int remoteOk = PCJ.get(i, "okCells");
+                if (remoteOk != localN) {
+                    System.out.println("Verification failed for thread #" + i);
+                }
+            }
+        }
     }
 
     private static class RandomForRA {
@@ -189,8 +216,8 @@ public class RandomAccessNew implements StartPoint {
         public static final long PERIOD = 1317624576693539401L;
         private long rand;
 
-        public RandomForRA() {
-            int n = PCJ.myId() * 4;
+        public RandomForRA(int initialVal) {
+            long n = initialVal * 4;
             int i, j;
             /* unsigned */ long[] m2 = new /* unsigned */ long[64];
             /* unsigned */ long temp, ran;
