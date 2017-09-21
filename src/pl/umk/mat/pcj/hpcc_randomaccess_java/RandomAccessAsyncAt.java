09304dc4c1f788a4c86ebe41c1c99ed9051da23d
Łukasz Górski <lukeg5@gmail.com>
2017-09-22 00:51:54 +0200

    wersja z asyncat bez buforowania

--- src/pl/umk/mat/pcj/hpcc_randomaccess_java/RandomAccessAsyncAt.java
+++ src/pl/umk/mat/pcj/hpcc_randomaccess_java/RandomAccessAsyncAt.java
@@ -14,10 +14,10 @@ import org.pcj.RegisterStorage;
 import org.pcj.StartPoint;
 import org.pcj.Storage;
 
-@RegisterStorage(RandomAccessNew.Shared.class)
-public class RandomAccessNew implements StartPoint {
+@RegisterStorage(RandomAccessAsyncAt.Shared.class)
+public class RandomAccessAsyncAt implements StartPoint {
 
-    @Storage (RandomAccessNew.class)
+    @Storage (RandomAccessAsyncAt.class)
     enum Shared {
 
         table, receivedUpdates, test, okCells, executed, updatesShared
@@ -72,8 +72,7 @@ public class RandomAccessNew implements StartPoint {
             double stop = System.currentTimeMillis();
 
             PCJ.barrier();
-            PCJ.putLocal(this.preparedLocally, Shared.executed);
-            PCJ.waitFor(Shared.executed);
+
             System.out.println("Starting verification");
             verifyResultsLocally();
             thread0VerifyAll();
@@ -129,20 +128,8 @@ public class RandomAccessNew implements StartPoint {
         return (int) (rand & (localN - 1));
     }
 
-    private List<Long> generateRemoteUpdates(int update, int CHUNK_SIZE) {
-        List<Long> numbers = new ArrayList<>();
-
-        if (isTimeBound() && shutDown) {
-            return numbers;
-        }
-
-        for (int k = 0; k < CHUNK_SIZE && update + k < localUpdates; k++) {
-            long rand = random.nextLong();
-            numbers.add(rand);
-        }
-
-        preparedLocally += numbers.size();
-        return numbers;
+    private long generateRemoteUpdate() {
+            return random.nextLong();
     }
 
     boolean isTimeBound() {
@@ -151,185 +138,38 @@ public class RandomAccessNew implements StartPoint {
 
     private void performRandomAccess() {
         long timeBoundStart = System.currentTimeMillis();
-        int iter = 0;
-        for (int update = 0; update < localUpdates; update += BUFFERED_UPDATES) {
-            List<Long> updateList = generateRemoteUpdates(update, BUFFERED_UPDATES);
+        for (int update = 0; update < localUpdates; update ++) {
+            long randomLocation =  generateRemoteUpdate();
 
             if (isTimeBound()) {
-                if (PCJ.myId() == 0) {
                     shutDown = System.currentTimeMillis() - timeBoundStart > timeBoundSeconds * 1e3;
-                }
             }
 
-            //updateList = alltoallHypercube(updateList, iter++);
-            updateList = alltoallBlocking(updateList, iter++);
-            //updateList = allToAllNonBlocking(updateList, iter++);
-            performUpdates(updateList);
+            if (whichPE(randomLocation) != PCJ.myId()) {
+                int executedRemotely = PCJ.getLocal(Shared.executed);
+                PCJ.putLocal(executedRemotely + 1, Shared.executed);
+            }
+            
+            PCJ.asyncAt(whichPE(randomLocation), () -> {
+                updateSingleCell(randomLocation);
+            });
+            
+            
 
             if (isTimeBound()) {
                 if (shutDown) {
                     System.out.println("Shutting down");
-
                     break;
                 }
             }
         }
     }
 
-    private List<Long> prepareBlocks(List<Long> updates) {
-        Iterator<Long> iter = updates.iterator();
-        List<Long>[] updatesToAdd = (List<Long>[]) new ArrayList[PCJ.threadCount()];
-        for (int i = 0; i < updatesToAdd.length; i++) {
-            updatesToAdd[i] = new ArrayList<>();
-        }
-        while (iter.hasNext()) {
-            long val = iter.next();
-            int PENumber = whichPE(val);
-            if (PENumber != PCJ.myId()) {
-                updatesToAdd[PENumber].add(val);
-                iter.remove();
-            }
-        }
-        if (isTimeBound()) {
-            if (shutDown) {
-                for (int i = 0; i < updatesToAdd.length; i++) {
-                    updatesToAdd[i].add(POISON_PILL);
-                }
-            }
-        }
-        PCJ.putLocal(updatesToAdd, Shared.updatesShared);
-        return updates;
-    }
-
-    private List<Long> allToAllNonBlocking(List<Long> updates, int iterNo) {
-
-        PCJ.barrier();
-        updates = prepareBlocks(updates);
-        PCJ.barrier();
-        //prepare futures array
-        PcjFuture<List<Long>>[] futures = new PcjFuture[PCJ.threadCount()];
-
-        //get the data 
-        for (int image = (PCJ.myId() + 1) % PCJ.threadCount(), num = 0; num != PCJ.threadCount() - 1; image = (image + 1) % PCJ.threadCount()) {
-            if (image != PCJ.myId()) {
-                futures[image] = PCJ.asyncGet(image, Shared.updatesShared, PCJ.myId());
-            }
-            num++;
-        }
-
-        int numReceived = 0;
-        while (numReceived != PCJ.threadCount() - 1) {
-            for (int i = 0; i < futures.length; i++) {
-                if (futures[i] != null && futures[i].isDone()) {
-                    List<Long> recv = futures[i].get();
-                    updates.addAll(recv);
-                    numReceived++;
-                    futures[i] = null;
-                }
-            }
-        }
-        return updates;
-    }
-
-    private List<Long> alltoallBlocking(List<Long> updates, int iterNo) {
-        updates = prepareBlocks(updates);
-        PCJ.barrier();
-        for (int image = (PCJ.myId() + 1) % PCJ.threadCount(), num = 0; num != PCJ.threadCount() - 1; image = (image + 1) % PCJ.threadCount()) {
-            List<Long> recv = (List<Long>) PCJ.get(image, Shared.updatesShared, PCJ.myId());
-            updates.addAll(recv);
-            num++;
-        }
-        PCJ.barrier();
-        return updates;
-    }
-
-    private List<Long> alltoallHypercube(List<Long> updates, int iterNo) {
-        //all-to-all hypercube personalized communication, per 
-        //http://www.sandia.gov/~sjplimp/docs/cluster06.pdf, p. 5.
-        List<Long> send = new ArrayList<>();
-        List<Long> keep = new ArrayList<>();
-
-        for (int dimension = 0; dimension < logNumProcs; dimension++) {
-
-            int partner = (1 << dimension) ^ myId;
-
-            long mask = 1L << (logLocalN + dimension);
-
-            prepareUpdateLists(partner, updates, keep, send, mask);
-            sendListToPartner(send, partner, iterNo, dimension);
-            List<Long> received = receiveUpdates(iterNo, dimension);
-            updates = keep;
-            updates.addAll(received);
-            keep = new ArrayList<>();
-        }
-        //List<Long> received = receiveUpdates(logNumProcs);
-        //updates.addAll(received);
-        return updates;
-    }
-
-    private void sendListToPartner(List<Long> send, int partner, int iterNo, int dimension) throws ClassCastException {
-        PCJ.put(send, partner, Shared.receivedUpdates, iterNo, dimension);
-        send.clear();
-    }
-
-    private List<Long> receiveUpdates(int iterNo, int dimension) {
-        List<Long> received = null;
-        while (received == null) {
-            received = PCJ.getLocal(Shared.receivedUpdates, iterNo, dimension);
-        }
-        PCJ.putLocal(Shared.receivedUpdates, null, iterNo, dimension);
-        return received;
-    }
-
-    private void prepareUpdateLists(int partner, List<Long> updates1, List<Long> keep, List<Long> send, long mask) {
-        if (partner > myId) {
-            for (long update : updates1) {
-
-                if (update == POISON_PILL) {
-                    shutDown = true;
-                    continue;
-                }
-                if ((update & mask) != 0) {
-                    send.add(update);
-                } else {
-                    keep.add(update);
-                }
-            }
-        } else {
-            for (long update : updates1) {
-
-                if (update == POISON_PILL) {
-                    shutDown = true;
-                    continue;
-                }
-
-                if ((update & mask) != 0) {
-                    keep.add(update);
-                } else {
-                    send.add(update);
-                }
-            }
-        }
-
-        if (shutDown) {
-            keep.add(POISON_PILL);
-            send.add(POISON_PILL);
-        }
-    }
-
+   
     int whichPE(long pos) {
         return (int) (pos >> logLocalN) & (threadCount - 1);
     }
 
-    private void performUpdates(List<Long> updateList) {
-        for (long update : updateList) {
-            if (update == POISON_PILL) {
-                shutDown = true;
-            } else {
-                updateSingleCell(update);
-            }
-        }
-    }
 
     private void updateSingleCell(long update) throws ClassCastException {
         int localOffset = randomNumberToLocalPosition(update);
